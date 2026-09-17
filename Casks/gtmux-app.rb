ba4ed57642c5dd1a6575ba4a0bb04642ee3489ba
chenchaoyi <gtmux@ccy.dev>
2026-09-17 00:18:19 +0000

    gtmux-app 1.0.29

--- Casks/gtmux-app.rb
+++ Casks/gtmux-app.rb
@@ -1,7 +1,7 @@
 # Published by gtmux's release workflow. DO NOT EDIT.
 cask "gtmux-app" do
-  version "1.0.28"
-  sha256 "e46d76532464584c5754b7eeb3553f3c2bb4ac4e166ae3491ec9989ad602de17"
+  version "1.0.29"
+  sha256 "a2a192140cb79279d36745f676670394f6b60ae2dfa11eb8a281db98a1238cc9"
 
   url "https://github.com/chenchaoyi/gtmux/releases/download/v#{version}/Gtmux-#{version}-macos.zip"
   name "Gtmux"
